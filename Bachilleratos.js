3e6bcc4fadcf666f6297c2cf8fb56d9a967aecb5
Kevin <124392964+K3viiin@users.noreply.github.com>
2023-09-07 12:37:07 -0600

    up

--- Bachilleratos.js
+++ Bachilleratos.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Elementos
 
 let logo = document.querySelector(".logo");
@@ -15,22 +14,4 @@ const darkMode = ()=>{
 
 logo.addEventListener("click",(r)=>{
     // redirigir();
-=======
-// Elementos
-
-let logo = document.querySelector(".logo");
-
-const redirigir = ()=>{
-    location.href = "https://www.facebook.com/people/Avance-Insa/100013071965296/";
-}
-
-const darkMode = ()=>{
-    classList.add ()
-}
-
-// Codigo
-
-logo.addEventListener("click",(r)=>{
-    // redirigir();
->>>>>>> 02fabf3dfde7b0a0dfedd2a80b6239a322d2b9c6
 })
