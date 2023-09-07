072158343c0e5c2379a21fdacc5941c829d3cf9e
C-E-INSA <143138530+C-E-INSA@users.noreply.github.com>
2023-09-07 09:39:35 -0600

    Add files via upload

---
+++ Bachilleratos.js
@@ -0,0 +1,36 @@
+<<<<<<< HEAD
+// Elementos
+
+let logo = document.querySelector(".logo");
+
+const redirigir = ()=>{
+    location.href = "https://www.facebook.com/people/Avance-Insa/100013071965296/";
+}
+
+const darkMode = ()=>{
+    classList.add ()
+}
+
+// Codigo
+
+logo.addEventListener("click",(r)=>{
+    // redirigir();
+=======
+// Elementos
+
+let logo = document.querySelector(".logo");
+
+const redirigir = ()=>{
+    location.href = "https://www.facebook.com/people/Avance-Insa/100013071965296/";
+}
+
+const darkMode = ()=>{
+    classList.add ()
+}
+
+// Codigo
+
+logo.addEventListener("click",(r)=>{
+    // redirigir();
+>>>>>>> 02fabf3dfde7b0a0dfedd2a80b6239a322d2b9c6
+})
